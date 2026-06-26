576d5ed24498d58cbd815eee8356d45a14347bc2
26039088 <mphanganemalesele008@gmail.com>
2026-06-26 16:28:51 +0200

    first commit

---
+++ src/main/java/com/mycompany/namedisplayer/NameDisplayer.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.namedisplayer;
+     import javax.swing.JOptionPane;
+public class NameDisplayer {
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null,"My Name Is jim");
+       JOptionPane.showMessageDialog(null,"My Surname Is Mphangane");
+JOptionPane.showMessageDialog(null,"I am in my first year at The University Of Venda.");
+    }
+}
